9b544a07e665baaebd21d5d9340790614aaec9d5
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-30 14:50:54 -0400

    Add Calculator component (initial)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Retirement Calculator 2.0</p>
-      </header>
+        </header>
+        <Calculator/>
     </div>
   );
 }
---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Calculator = () => {
+
+    return (
+        <div>
+            <h2>Calculator</h2>
+        </div>
+    )
+};
+
+export default Calculator;
